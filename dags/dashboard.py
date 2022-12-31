0b605d471a2e8bb79b137dbf69080836d8d0671d
ahmednadr <ahmednader.an5@gmail.com>
2022-12-31 21:00:49 +0200

    first dashboard iteration

---
+++ dags/dashboard.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import dash
+from dash import html
+from dash import dcc
+import dash_bootstrap_components as dbc
+from dash_bootstrap_templates import load_figure_template
+import plotly.graph_objs as go
+import plotly.express as px
+from dash.dependencies import Input,Output
+from dash import callback_context
+
+
+load_figure_template('LITERA')
+
+
+###--------------Build the figures / dropdowns------------------------------------
+
+x = np.random.sample(100)
+y = np.random.sample(100)
+z = np.random.choice(a = ['a','b','c'], size = 100)
+
+
+df1 = pd.DataFrame({'x': x, 'y':y, 'z':z}, index = range(100))
+
+fig1 = px.scatter(df1, x= x, y = y, color = z)
+
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "24rem",
+    "padding": "2rem 1rem",
+    "background-color": "darkslategray",
+    # "background-color": "#2C3639",
+    "color":"white"
+}
+
+
+sidebar = html.Div(
+    [
+        html.H2("Filters"),
+        html.Hr(),
+        # html.P(
+        #     "A simple sidebar layout with filters", className="lead"
+        # ),
+        dbc.Nav(
+            [
+                dcc.Dropdown(id = 'one'),
+                html.Br(),
+                dcc.Dropdown(id = 'two'),
+                html.Br(),
+                dcc.Dropdown(id = 'three')
+
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+
+###---------------Create the layout of the app ------------------------
+
+app = dash.Dash(external_stylesheets=[dbc.themes.LITERA])
+
+app.layout = html.Div(children = [
+                dbc.Row([
+                    dbc.Col(),
+
+                    dbc.Col(html.H1('The 1991 accidents report'),width = 8, style = {'margin-left':'15px','margin-top':'25px'})
+                    ]),
+                dbc.Row(
+                    [dbc.Col(sidebar),
+                    dbc.Col(dcc.Graph(id = 'graph1', figure = fig1), width = 8, style = {'margin-left':'15px', 'margin-top':'7px', 'margin-right':'15px'})
+                    ])
+    ],
+    # style={"background-color": "#2C3639"}
+)
+
+
+
+if __name__ == '__main__':
+    app.run_server(port = 9001, debug=True)
